aabf4d06ca646bf572d5b678ab1bf86709a673b9
Даша Завтрак <zavdariag@gmail.com>
2024-11-17 13:00:34 +0200

    Form was added, editing language, added return book button

---
+++ libraryMeneger/UserForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace libraryMeneger
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PersonalInfoLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SurnameLabel = new System.Windows.Forms.Label();
+            this.PhoneLabel = new System.Windows.Forms.Label();
+            this.MailLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.BookListBox = new System.Windows.Forms.ListBox();
+            this.ReservedButton = new System.Windows.Forms.Button();
+            this.EditProfilButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PersonalInfoLabel
+            // 
+            this.PersonalInfoLabel.AutoSize = true;
+            this.PersonalInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PersonalInfoLabel.Location = new System.Drawing.Point(12, 24);
+            this.PersonalInfoLabel.Name = "PersonalInfoLabel";
+            this.PersonalInfoLabel.Size = new System.Drawing.Size(132, 25);
+            this.PersonalInfoLabel.TabIndex = 0;
+            this.PersonalInfoLabel.Text = "Personal Info:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Surname:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phone number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Mail:";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.NameLabel.Location = new System.Drawing.Point(146, 73);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(46, 16);
+            this.NameLabel.TabIndex = 6;
+            this.NameLabel.Text = "no info";
+            // 
+            // SurnameLabel
+            // 
+            this.SurnameLabel.AutoSize = true;
+            this.SurnameLabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.SurnameLabel.Location = new System.Drawing.Point(146, 110);
+            this.SurnameLabel.Name = "SurnameLabel";
+            this.SurnameLabel.Size = new System.Drawing.Size(46, 16);
+            this.SurnameLabel.TabIndex = 7;
+            this.SurnameLabel.Text = "no info";
+            // 
+            // PhoneLabel
+            // 
+            this.PhoneLabel.AutoSize = true;
+            this.PhoneLabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.PhoneLabel.Location = new System.Drawing.Point(146, 144);
+            this.PhoneLabel.Name = "PhoneLabel";
+            this.PhoneLabel.Size = new System.Drawing.Size(46, 16);
+            this.PhoneLabel.TabIndex = 8;
+            this.PhoneLabel.Text = "no info";
+            // 
+            // MailLabel
+            // 
+            this.MailLabel.AutoSize = true;
+            this.MailLabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.MailLabel.Location = new System.Drawing.Point(146, 182);
+            this.MailLabel.Name = "MailLabel";
+            this.MailLabel.Size = new System.Drawing.Size(46, 16);
+            this.MailLabel.TabIndex = 9;
+            this.MailLabel.Text = "no info";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.UsernameLabel.Location = new System.Drawing.Point(327, 33);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(46, 16);
+            this.UsernameLabel.TabIndex = 10;
+            this.UsernameLabel.Text = "no info";
+            // 
+            // BookListBox
+            // 
+            this.BookListBox.FormattingEnabled = true;
+            this.BookListBox.ItemHeight = 16;
+            this.BookListBox.Location = new System.Drawing.Point(24, 227);
+            this.BookListBox.Name = "BookListBox";
+            this.BookListBox.Size = new System.Drawing.Size(349, 148);
+            this.BookListBox.TabIndex = 11;
+            // 
+            // ReservedButton
+            // 
+            this.ReservedButton.Location = new System.Drawing.Point(24, 381);
+            this.ReservedButton.Name = "ReservedButton";
+            this.ReservedButton.Size = new System.Drawing.Size(287, 45);
+            this.ReservedButton.TabIndex = 12;
+            this.ReservedButton.Text = "Reserved a book";
+            this.ReservedButton.UseVisualStyleBackColor = true;
+            // 
+            // EditProfilButton
+            // 
+            this.EditProfilButton.Location = new System.Drawing.Point(317, 381);
+            this.EditProfilButton.Name = "EditProfilButton";
+            this.EditProfilButton.Size = new System.Drawing.Size(56, 45);
+            this.EditProfilButton.TabIndex = 13;
+            this.EditProfilButton.Text = "Edit profile";
+            this.EditProfilButton.UseVisualStyleBackColor = true;
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 447);
+            this.Controls.Add(this.EditProfilButton);
+            this.Controls.Add(this.ReservedButton);
+            this.Controls.Add(this.BookListBox);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.MailLabel);
+            this.Controls.Add(this.PhoneLabel);
+            this.Controls.Add(this.SurnameLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PersonalInfoLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "UserForm";
+            this.Text = "UserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PersonalInfoLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label SurnameLabel;
+        private System.Windows.Forms.Label PhoneLabel;
+        private System.Windows.Forms.Label MailLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.ListBox BookListBox;
+        private System.Windows.Forms.Button ReservedButton;
+        private System.Windows.Forms.Button EditProfilButton;
+    }
+}
