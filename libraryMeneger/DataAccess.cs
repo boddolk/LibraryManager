5935d56063a4ab0f6cd76178a8038d07ad5df1a6
Valentyn <valikkican5@gmail.com>
2024-11-17 12:26:21 +0200

    Start adding the connection class

---
+++ libraryMeneger/DataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.IO;
+
+namespace libraryMeneger
+{
+    public static class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+            
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string dbPath = Path.Combine(folderPath, "BooksAndUsers.db");
+
+           
+            if (!File.Exists(dbPath))
+            {
+                File.Create(dbPath).Dispose();
+            }
+
+            
+            using (var db = new SqliteConnection($"Filename={dbPath}"))
+            {
+                db.Open();
+
+                string tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
+                    "Text_Entry NVARCHAR(2048) NULL)";
+
+                var createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
