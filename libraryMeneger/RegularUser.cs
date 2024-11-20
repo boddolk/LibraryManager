5d790cfec33b562751033c9613271f5c44e9fb6a
Даша Завтрак <zavdariag@gmail.com>
2024-11-20 11:34:56 +0200

    Hierarchy added

---
+++ libraryMeneger/RegularUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryMeneger.user
+{
+    internal class RegularUser:BaseUser
+
+    {
+        public RegularUser(string myName, string mySurname, string myPassword, string myEmail, string myPhoneNumber)
+            : base(myName, mySurname, myPassword, myEmail, myPhoneNumber)
+        {
+            IsAdmin = false;
+        }
+    }
+}
