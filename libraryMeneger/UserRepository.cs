b9afaba504620d38f06ec7c6a38dfc5af8be264f
Valentyn <valikkican5@gmail.com>
2024-11-21 22:20:17 +0200

    User repository was created and methods for loggining added (existing of user, his role, correct password ). User repository has its own interface and implementation and was created for convinient access to data.

---
+++ libraryMeneger/UserRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace libraryMeneger.Data.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private string dbPath;
+        private SqliteConnection connection;
+        public UserRepository(string DBFileName)
+        {
+            dbPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), DBFileName);
+            connection = new SqliteConnection($"Data Source={dbPath};Connect Timeout=600");
+        }
+        
+        public override bool DoesSuchUserExist(string loginToCheck)
+        {
+            try
+            {
+                connection.Open();       
+            
+            string query = "SELECT EXISTS(SELECT 1 FROM UsersAndBooks WHERE Login = @Value)";
+
+            using ( var command = new SqliteCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Value", loginToCheck);
+                var result = (long)command.ExecuteScalar();               
+                    return result == 1;
+              
+            }
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+        public override bool IsGivenUserAdmin(string loginToCheck)
+        {
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT IsAdmin FROM UsersAndBooks WHERE Login = @Value";
+
+                using (var command = new SqliteCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@Value", loginToCheck);
+
+                    var result = command.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        return Convert.ToBoolean(result);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        
+        public override bool IsPasswordCorrect(string loginToCheck, string Password)
+        {
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT COUNT(1) FROM UsersAndBooks WHERE Login = @Login AND Password = @Password";
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Login", loginToCheck);
+                    command.Parameters.AddWithValue("@Password", Password);
+
+                    var result = (long)command.ExecuteScalar();
+                    return result == 1;
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
