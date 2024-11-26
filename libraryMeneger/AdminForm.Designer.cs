7d8a51e42bdb5e88324d4d834b465c94f47f49db
Bohdan Dobrotvor <dobrotvorbo@gmail.com>
2024-11-26 17:20:42 +0200

    Aded general view

--- libraryMeneger/AdminForm.Designer.cs
+++ libraryMeneger/AdminForm.Designer.cs
@@ -36,15 +36,14 @@
             this.NameLabel = new System.Windows.Forms.Label();
             this.AuthorLabel = new System.Windows.Forms.Label();
             this.ArticleLabel = new System.Windows.Forms.Label();
-            this.StatusLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.AuthorTextBox = new System.Windows.Forms.TextBox();
-            this.NameTextBox = new System.Windows.Forms.TextBox();
-            this.ArticleTextBox = new System.Windows.Forms.TextBox();
-            this.YearNumer = new System.Windows.Forms.NumericUpDown();
-            this.StatusComboBox = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.YearNumer)).BeginInit();
+            this.currentArticleLabel = new System.Windows.Forms.Label();
+            this.currenttitleLabel = new System.Windows.Forms.Label();
+            this.currentAuthorLabel = new System.Windows.Forms.Label();
+            this.currentYearLabel = new System.Windows.Forms.Label();
+            this.currentStatusLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // GiveOutButton
@@ -95,7 +94,7 @@
             // NameLabel
             // 
             this.NameLabel.AutoSize = true;
-            this.NameLabel.Location = new System.Drawing.Point(12, 102);
+            this.NameLabel.Location = new System.Drawing.Point(51, 102);
             this.NameLabel.Name = "NameLabel";
             this.NameLabel.Size = new System.Drawing.Size(36, 16);
             this.NameLabel.TabIndex = 5;
@@ -104,7 +103,7 @@
             // AuthorLabel
             // 
             this.AuthorLabel.AutoSize = true;
-            this.AuthorLabel.Location = new System.Drawing.Point(12, 134);
+            this.AuthorLabel.Location = new System.Drawing.Point(51, 134);
             this.AuthorLabel.Name = "AuthorLabel";
             this.AuthorLabel.Size = new System.Drawing.Size(48, 16);
             this.AuthorLabel.TabIndex = 6;
@@ -113,65 +112,20 @@
             // ArticleLabel
             // 
             this.ArticleLabel.AutoSize = true;
-            this.ArticleLabel.Location = new System.Drawing.Point(12, 72);
+            this.ArticleLabel.Location = new System.Drawing.Point(51, 72);
             this.ArticleLabel.Name = "ArticleLabel";
             this.ArticleLabel.Size = new System.Drawing.Size(47, 16);
             this.ArticleLabel.TabIndex = 7;
             this.ArticleLabel.Text = "Article:";
             // 
-            // StatusLabel
-            // 
-            this.StatusLabel.AutoSize = true;
-            this.StatusLabel.Location = new System.Drawing.Point(13, 196);
-            this.StatusLabel.Name = "StatusLabel";
-            this.StatusLabel.Size = new System.Drawing.Size(47, 16);
-            this.StatusLabel.TabIndex = 8;
-            this.StatusLabel.Text = "Status:";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 164);
+            this.label1.Location = new System.Drawing.Point(51, 164);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 16);
             this.label1.TabIndex = 9;
             this.label1.Text = "Year:";
-            // 
-            // AuthorTextBox
-            // 
-            this.AuthorTextBox.Location = new System.Drawing.Point(105, 130);
-            this.AuthorTextBox.Name = "AuthorTextBox";
-            this.AuthorTextBox.Size = new System.Drawing.Size(683, 22);
-            this.AuthorTextBox.TabIndex = 11;
-            // 
-            // NameTextBox
-            // 
-            this.NameTextBox.Location = new System.Drawing.Point(105, 99);
-            this.NameTextBox.Name = "NameTextBox";
-            this.NameTextBox.Size = new System.Drawing.Size(683, 22);
-            this.NameTextBox.TabIndex = 12;
-            // 
-            // ArticleTextBox
-            // 
-            this.ArticleTextBox.Location = new System.Drawing.Point(105, 68);
-            this.ArticleTextBox.Name = "ArticleTextBox";
-            this.ArticleTextBox.Size = new System.Drawing.Size(683, 22);
-            this.ArticleTextBox.TabIndex = 13;
-            // 
-            // YearNumer
-            // 
-            this.YearNumer.Location = new System.Drawing.Point(105, 162);
-            this.YearNumer.Name = "YearNumer";
-            this.YearNumer.Size = new System.Drawing.Size(683, 22);
-            this.YearNumer.TabIndex = 14;
-            // 
-            // StatusComboBox
-            // 
-            this.StatusComboBox.FormattingEnabled = true;
-            this.StatusComboBox.Location = new System.Drawing.Point(105, 193);
-            this.StatusComboBox.Name = "StatusComboBox";
-            this.StatusComboBox.Size = new System.Drawing.Size(683, 24);
-            this.StatusComboBox.TabIndex = 15;
             // 
             // button1
             // 
@@ -182,17 +136,71 @@
             this.button1.Text = "returned book";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // currentArticleLabel
+            // 
+            this.currentArticleLabel.AutoSize = true;
+            this.currentArticleLabel.Location = new System.Drawing.Point(154, 72);
+            this.currentArticleLabel.Name = "currentArticleLabel";
+            this.currentArticleLabel.Size = new System.Drawing.Size(84, 16);
+            this.currentArticleLabel.TabIndex = 17;
+            this.currentArticleLabel.Text = "Empty article";
+            // 
+            // currenttitleLabel
+            // 
+            this.currenttitleLabel.AutoSize = true;
+            this.currenttitleLabel.Location = new System.Drawing.Point(154, 102);
+            this.currenttitleLabel.Name = "currenttitleLabel";
+            this.currenttitleLabel.Size = new System.Drawing.Size(68, 16);
+            this.currenttitleLabel.TabIndex = 18;
+            this.currenttitleLabel.Text = "Empty title";
+            // 
+            // currentAuthorLabel
+            // 
+            this.currentAuthorLabel.AutoSize = true;
+            this.currentAuthorLabel.Location = new System.Drawing.Point(154, 134);
+            this.currentAuthorLabel.Name = "currentAuthorLabel";
+            this.currentAuthorLabel.Size = new System.Drawing.Size(85, 16);
+            this.currentAuthorLabel.TabIndex = 19;
+            this.currentAuthorLabel.Text = "Empty author";
+            // 
+            // currentYearLabel
+            // 
+            this.currentYearLabel.AutoSize = true;
+            this.currentYearLabel.Location = new System.Drawing.Point(154, 164);
+            this.currentYearLabel.Name = "currentYearLabel";
+            this.currentYearLabel.Size = new System.Drawing.Size(75, 16);
+            this.currentYearLabel.TabIndex = 20;
+            this.currentYearLabel.Text = "Empty year";
+            // 
+            // currentStatusLabel
+            // 
+            this.currentStatusLabel.AutoSize = true;
+            this.currentStatusLabel.Location = new System.Drawing.Point(154, 196);
+            this.currentStatusLabel.Name = "currentStatusLabel";
+            this.currentStatusLabel.Size = new System.Drawing.Size(83, 16);
+            this.currentStatusLabel.TabIndex = 21;
+            this.currentStatusLabel.Text = "Empty status";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(52, 196);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(47, 16);
+            this.StatusLabel.TabIndex = 8;
+            this.StatusLabel.Text = "Status:";
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currentStatusLabel);
+            this.Controls.Add(this.currentYearLabel);
+            this.Controls.Add(this.currentAuthorLabel);
+            this.Controls.Add(this.currenttitleLabel);
+            this.Controls.Add(this.currentArticleLabel);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.StatusComboBox);
-            this.Controls.Add(this.YearNumer);
-            this.Controls.Add(this.ArticleTextBox);
-            this.Controls.Add(this.NameTextBox);
-            this.Controls.Add(this.AuthorTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.StatusLabel);
             this.Controls.Add(this.ArticleLabel);
@@ -207,7 +215,6 @@
             this.Text = "AdminForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AdminForm_FormClosing);
             this.Load += new System.EventHandler(this.AdminForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.YearNumer)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -223,13 +230,13 @@
         private System.Windows.Forms.Label NameLabel;
         private System.Windows.Forms.Label AuthorLabel;
         private System.Windows.Forms.Label ArticleLabel;
-        private System.Windows.Forms.Label StatusLabel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox AuthorTextBox;
-        private System.Windows.Forms.TextBox NameTextBox;
-        private System.Windows.Forms.TextBox ArticleTextBox;
-        private System.Windows.Forms.NumericUpDown YearNumer;
-        private System.Windows.Forms.ComboBox StatusComboBox;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label currentArticleLabel;
+        private System.Windows.Forms.Label currenttitleLabel;
+        private System.Windows.Forms.Label currentAuthorLabel;
+        private System.Windows.Forms.Label currentYearLabel;
+        private System.Windows.Forms.Label currentStatusLabel;
+        private System.Windows.Forms.Label StatusLabel;
     }
 }
