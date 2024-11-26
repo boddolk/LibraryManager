fbaa592e7509aa952eba5d4f48a4741c4a80a279
Даша Завтрак <zavdariag@gmail.com>
2024-11-26 17:24:42 +0200

    AddForm EditForm base

--- libraryMeneger/AdminForm.cs
+++ libraryMeneger/AdminForm.cs
@@ -7,32 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using libraryMeneger.Data.BookRepository;
 using libraryMeneger.user;
 
 namespace libraryMeneger
 {
     public partial class AdminForm : Form
     {
+        AdminUser currentUser;
+        BooksRepository repository = new BooksRepository("UsersAndBooks.db");
         public AdminForm(AdminUser user)
         {
+            
             InitializeComponent();
             this.Text = "Admin - " + user.Login;
+            currentUser = user;
         }
 
         private void AddButton_Click(object sender, EventArgs e)
         {
-           
+           BookAddForm bookAddForm = new BookAddForm(currentUser);
+            bookAddForm.Show();
 
         }
 
         private void AdminForm_Load(object sender, EventArgs e)
         {
-
+          
         }
 
         private void AdminForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            //замінити
+            int arcticle = Convert.ToInt32(ArticleTextBox.Text);
+            BookEditForm form = new BookEditForm(repository.getBook(arcticle), currentUser);
+            form.Show();
+            this.Close();
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ libraryMeneger/BookEditForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libraryMeneger.book;
+using libraryMeneger.user;
+
+namespace libraryMeneger
+{
+    public partial class BookEditForm : Form
+    {
+        GenBook currentBook;
+        AdminUser currentUser;
+        public BookEditForm(GenBook book, AdminUser user)
+        {
+            InitializeComponent();
+            currentBook = book;
+            currentUser = user;
+
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
