6effcf80b1747dd3e17d1d7572dea0f2132f83d4
Bohdan Dobrotvor <dobrotvorbo@gmail.com>
2024-11-27 22:44:59 +0200

    Aded the frame of the return form functions

--- libraryMeneger/ReturnBookForm.Designer.cs
+++ libraryMeneger/ReturnBookForm.Designer.cs
@@ -31,18 +31,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.returnMenuButton = new System.Windows.Forms.Button();
-            this.currentDateLabel = new System.Windows.Forms.Label();
+            this.cStartDatelabel = new System.Windows.Forms.Label();
             this.filterButton = new System.Windows.Forms.Button();
             this.filtParemetrLabel = new System.Windows.Forms.Label();
             this.filterTextBox = new System.Windows.Forms.TextBox();
-            this.reserveComboBox = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.issuedComboBox = new System.Windows.Forms.ComboBox();
+            this.cEndDatelabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 143);
+            this.label2.Location = new System.Drawing.Point(31, 143);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 16);
             this.label2.TabIndex = 17;
@@ -51,7 +52,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 185);
+            this.label1.Location = new System.Drawing.Point(32, 185);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 16);
             this.label1.TabIndex = 16;
@@ -59,26 +60,28 @@
             // 
             // returnMenuButton
             // 
-            this.returnMenuButton.Location = new System.Drawing.Point(472, 168);
+            this.returnMenuButton.Location = new System.Drawing.Point(365, 168);
             this.returnMenuButton.Name = "returnMenuButton";
             this.returnMenuButton.Size = new System.Drawing.Size(111, 36);
             this.returnMenuButton.TabIndex = 15;
             this.returnMenuButton.Text = "Return";
             this.returnMenuButton.UseVisualStyleBackColor = true;
+            this.returnMenuButton.Click += new System.EventHandler(this.returnMenuButton_Click);
             // 
-            // currentDateLabel
+            // cStartDatelabel
             // 
-            this.currentDateLabel.AutoSize = true;
-            this.currentDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.currentDateLabel.Location = new System.Drawing.Point(98, 136);
-            this.currentDateLabel.Name = "currentDateLabel";
-            this.currentDateLabel.Size = new System.Drawing.Size(113, 24);
-            this.currentDateLabel.TabIndex = 13;
-            this.currentDateLabel.Text = "Current date";
+            this.cStartDatelabel.AutoSize = true;
+            this.cStartDatelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cStartDatelabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.cStartDatelabel.Location = new System.Drawing.Point(103, 136);
+            this.cStartDatelabel.Name = "cStartDatelabel";
+            this.cStartDatelabel.Size = new System.Drawing.Size(161, 24);
+            this.cStartDatelabel.TabIndex = 13;
+            this.cStartDatelabel.Text = "Start date is empty";
             // 
             // filterButton
             // 
-            this.filterButton.Location = new System.Drawing.Point(472, 72);
+            this.filterButton.Location = new System.Drawing.Point(477, 72);
             this.filterButton.Name = "filterButton";
             this.filterButton.Size = new System.Drawing.Size(111, 34);
             this.filterButton.TabIndex = 12;
@@ -88,7 +91,7 @@
             // filtParemetrLabel
             // 
             this.filtParemetrLabel.AutoSize = true;
-            this.filtParemetrLabel.Location = new System.Drawing.Point(27, 81);
+            this.filtParemetrLabel.Location = new System.Drawing.Point(32, 81);
             this.filtParemetrLabel.Name = "filtParemetrLabel";
             this.filtParemetrLabel.Size = new System.Drawing.Size(48, 16);
             this.filtParemetrLabel.TabIndex = 11;
@@ -96,43 +99,56 @@
             // 
             // filterTextBox
             // 
-            this.filterTextBox.Location = new System.Drawing.Point(81, 78);
+            this.filterTextBox.Location = new System.Drawing.Point(86, 78);
             this.filterTextBox.Name = "filterTextBox";
             this.filterTextBox.Size = new System.Drawing.Size(333, 22);
             this.filterTextBox.TabIndex = 10;
             // 
-            // reserveComboBox
+            // issuedComboBox
             // 
-            this.reserveComboBox.FormattingEnabled = true;
-            this.reserveComboBox.Location = new System.Drawing.Point(30, 30);
-            this.reserveComboBox.Name = "reserveComboBox";
-            this.reserveComboBox.Size = new System.Drawing.Size(553, 24);
-            this.reserveComboBox.TabIndex = 9;
+            this.issuedComboBox.FormattingEnabled = true;
+            this.issuedComboBox.Location = new System.Drawing.Point(35, 30);
+            this.issuedComboBox.Name = "issuedComboBox";
+            this.issuedComboBox.Size = new System.Drawing.Size(553, 24);
+            this.issuedComboBox.TabIndex = 9;
+            this.issuedComboBox.SelectedIndexChanged += new System.EventHandler(this.issuedComboBox_SelectedIndexChanged);
             // 
-            // label3
+            // cEndDatelabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(98, 178);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 24);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "Current date";
+            this.cEndDatelabel.AutoSize = true;
+            this.cEndDatelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cEndDatelabel.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.cEndDatelabel.Location = new System.Drawing.Point(103, 179);
+            this.cEndDatelabel.Name = "cEndDatelabel";
+            this.cEndDatelabel.Size = new System.Drawing.Size(160, 24);
+            this.cEndDatelabel.TabIndex = 18;
+            this.cEndDatelabel.Text = "End date is empty";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(477, 168);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(111, 36);
+            this.exitButton.TabIndex = 19;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // ReturnBookForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 238);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(623, 238);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.cEndDatelabel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.returnMenuButton);
-            this.Controls.Add(this.currentDateLabel);
+            this.Controls.Add(this.cStartDatelabel);
             this.Controls.Add(this.filterButton);
             this.Controls.Add(this.filtParemetrLabel);
             this.Controls.Add(this.filterTextBox);
-            this.Controls.Add(this.reserveComboBox);
+            this.Controls.Add(this.issuedComboBox);
             this.Name = "ReturnBookForm";
             this.Text = "ReturnBookForm";
             this.ResumeLayout(false);
@@ -145,11 +161,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button returnMenuButton;
-        private System.Windows.Forms.Label currentDateLabel;
+        private System.Windows.Forms.Label cStartDatelabel;
         private System.Windows.Forms.Button filterButton;
         private System.Windows.Forms.Label filtParemetrLabel;
         private System.Windows.Forms.TextBox filterTextBox;
-        private System.Windows.Forms.ComboBox reserveComboBox;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox issuedComboBox;
+        private System.Windows.Forms.Label cEndDatelabel;
+        private System.Windows.Forms.Button exitButton;
     }
 }
--- libraryMeneger/ReturnBookForm.cs
+++ libraryMeneger/ReturnBookForm.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using libraryMeneger.book;
+using libraryMeneger.Data.BookRepository;
+using libraryMeneger.Data.BorrowHistory;
+using libraryMeneger.Data.StatusRepository;
+using libraryMeneger.user;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,105 @@ namespace libraryMeneger
 {
     public partial class ReturnBookForm : Form
     {
-        public ReturnBookForm()
+        AdminUser adminUser;
+        BooksRepository repository = new BooksRepository();
+        StatusRepository statRepository = new StatusRepository();
+        BorrowHistoryRepository borrowRepository = new BorrowHistoryRepository();
+
+        public ReturnBookForm(AdminUser user)
         {
             InitializeComponent();
+
+            adminUser = user;
+
+            string Title;
+            string UserLogin;
+            List<BookStatManager> issuedBooks = statRepository.getIssuedBookInfo();
+
+            if (issuedBooks != null)
+            {
+                this.issuedComboBox.Items.Insert(0, "Select the issued:");
+                for (int i = 0; i < issuedBooks.Count; i++)
+                {
+                    Title = repository.getBook(issuedBooks[i].Article).Title;
+                    UserLogin = statRepository.getUserIDByArticle(issuedBooks[i].Article);
+                    this.issuedComboBox.Items.Insert(i + 1, issuedBooks[i].GetToStringStatus(Title, UserLogin));
+                }
+            }
+            else
+            {
+                this.issuedComboBox.Items.Add("The library has no issued books!");
+            }
+            this.issuedComboBox.SelectedIndex = 0;
+
+            //DateTime currentDate = DateTime.Now.Date;
+            //this.cStartDatelabel.Text = "Start date empty";
+            //this.cEndDatelabel.Text = "End date empty";
+        }
+
+        private void issuedComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.issuedComboBox.SelectedIndex != 0)
+            {
+                int article = int.Parse(new string(this.issuedComboBox.SelectedItem.ToString().TakeWhile(char.IsDigit).ToArray()));
+                BookStatManager currentIssuedBook = new BookStatManager(); // затичка!!!!!!!!!!!!!!
+                DateTime startDate = currentIssuedBook.StartDate;
+                DateTime endDate = currentIssuedBook.EndDate;
+
+                this.cStartDatelabel.Text = startDate.ToString("dd.MM.yyyy");
+                this.cEndDatelabel.Text = endDate.ToString("dd.MM.yyyy");
+                
+                this.cStartDatelabel.ForeColor = Color.Black;
+                if (currentIssuedBook.EndDate > DateTime.Now.Date)
+                {
+                    this.cEndDatelabel.ForeColor = Color.Green;
+                }
+                else
+                {
+                    this.cEndDatelabel.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                this.cStartDatelabel.Text = "Start date is empty";
+                this.cEndDatelabel.Text = "End date is empty";
+                this.cStartDatelabel.ForeColor = Color.LightGray;
+                this.cEndDatelabel.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void returnMenuButton_Click(object sender, EventArgs e)
+        {
+            if (this.issuedComboBox.SelectedIndex != 0)
+            {
+                int article = int.Parse(new string(this.issuedComboBox.SelectedItem.ToString().TakeWhile(char.IsDigit).ToArray()));
+                BookStatManager currentIssuedBook = new BookStatManager(); // затичка!!!!!!!!!!!!!!
+                string Login = statRepository.getUserIDByArticle(article);
+                DateTime startDate = currentIssuedBook.StartDate;
+                DateTime endDate = currentIssuedBook.EndDate;
+                if (borrowRepository.addHistory(Login, article, currentIssuedBook.StartDate, DateTime.Now.Date))
+                {
+                    if (statRepository.removeBookWithItsStatus(article))
+                    {
+                        // Привітання
+                    }
+                }
+                else
+                {
+                    // деякі трабли
+                }
+            }
+            else
+            {
+                // виберіть книгу!!!
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            AdminForm form = new AdminForm(adminUser);
+            form.Show();
+            this.Close();
         }
     }
 }
