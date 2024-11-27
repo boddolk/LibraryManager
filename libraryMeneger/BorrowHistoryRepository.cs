aa5b190bb77c406ca37854c9137b4587544ce4c4
Valentyn <valikkican5@gmail.com>
2024-11-27 19:36:23 +0200

    Added method for adding to the history table. Method for changing status in StatusTable

--- libraryMeneger/BorrowHistoryRepository.cs
+++ libraryMeneger/BorrowHistoryRepository.cs
@@ -23,6 +23,35 @@ namespace libraryMeneger.Data.BorrowHistory
             dbPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), DBFileName);
             connection = new SQLiteConnection($"Data Source={dbPath};Connect Timeout=600");
         }
+        public override bool addHistory(string login, int article, DateTime borrowDate, DateTime returnDate)
+        {
+            try
+            {
+                connection.Open();
 
+
+                string query = "INSERT INTO Borrow_History (UserID, BookID, BorrowDate, ReturnDate) VALUES ( @LoginV, @ArticleV, @BorrowDateV, @ReturnDateV)";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@LoginV", login);
+                    command.Parameters.AddWithValue("@ArticleV", article);
+                    command.Parameters.AddWithValue("@BorrowDateV", borrowDate);
+                    command.Parameters.AddWithValue("@ReturnDateV", returnDate);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting item: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
